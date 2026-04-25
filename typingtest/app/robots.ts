7f1cc5d33edcf50eddd87e1f4346d201054da867
01-134231-071 <01-134231-071@student.bahria.edu.pk>
2026-04-26 02:49:29 +0500

    Fixed the history never[] array

--- typingtest/app/robots.ts
+++ typingtest/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://your-namecheap-domain.com/sitemap.xml', // Replace with your domain
+        sitemap: 'https://typingspeedtest.live/sitemap.xml', // Replace with your domain
     }
 }
--- typingtest/app/sitemap.ts
+++ typingtest/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://your-namecheap-domain.com', // Replace with your domain
+            url: 'https://typingspeedtest.live', // Replace with your domain
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 1,
